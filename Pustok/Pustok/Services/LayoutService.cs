7723d2c9aa921c23bbced4c7500d9ea93d7e07b9
Elchin Aghayev <Elchin@code.local>
2024-05-13 21:35:52 +0400

    project added

---
+++ Pustok/Pustok/Services/LayoutService.cs
@@ -0,0 +1,22 @@
+﻿using Pustok.Models;
+
+namespace Pustok.Services
+{
+    public class LayoutService
+    {
+        private readonly PustokDbContext _context;
+
+        public LayoutService(PustokDbContext context)
+        {
+            _context = context;
+        }
+        public List<Genre> GetGenres()
+        {
+            return _context.Genres.ToList();
+        }
+
+        public Dictionary<String,String> GetSettings() { 
+            return _context.Settings.ToDictionary(x=>x.Key, x => x.Value);  
+        }
+    }
+}
